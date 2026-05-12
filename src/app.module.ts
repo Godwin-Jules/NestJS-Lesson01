79c30e7a8d194af6730ea3cfd23bc8b5327ccb2d
Jules Dieu donné - G3-YG <mdjulesdeveloper23@gmail.com>
2026-05-12 12:41:23 +0000

    Creating the prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmployeesModule } from './employees/employees.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, EmployeesModule],
+  imports: [UsersModule, EmployeesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
