b2ca976af20a7b82e700547a89890d9d13fdbdc7
Jules Dieu donné - G3-YG <mdjulesdeveloper23@gmail.com>
2026-05-12 12:41:54 +0000

    Update employees dto names

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -8,8 +8,8 @@ import {
   Delete,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
-import { CreateEmployeeDto } from './dto/create-employee-dto';
-import { UpdateEmployeeDto } from './dto/update-employee-dto';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
 
 @Controller('employees')
 export class EmployeesController {
